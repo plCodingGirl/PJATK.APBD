d6ac6d68127f41d6519f4c7187d99a68e4be422c
maarchewkowa <k.guzewska@gmail.com>
2020-06-01 18:02:47 +0200

    Zestaw 11 - final

---
+++ Cw11/Pharmacy/Controllers/DoctorsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Models;
+
+namespace Pharmacy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly PharmacyDbContext _context;
+
+        public DoctorsController(PharmacyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Doctors
+        [HttpGet]
+        public async Task<IActionResult> GetDoctors()
+        {
+            return Ok(await _context.Doctors.ToListAsync());
+        }
+
+        // GET: api/Doctors/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDoctor(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(doctor);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditDoctor(int id, DoctorDto doctorDto)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            doctor.FirstName = doctorDto.FirstName;
+            doctor.LastName = doctorDto.LastName;
+            doctor.Email = doctorDto.Email;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor(DoctorDto doctorDto)
+        {
+            var doctor = new Doctor
+            {
+                FirstName = doctorDto.FirstName, 
+                LastName = doctorDto.LastName, 
+                Email = doctorDto.Email
+            };
+
+            await _context.Doctors.AddAsync(doctor);
+            await _context.SaveChangesAsync();
+
+            return Created("", doctor);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDoctor(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Doctors.Remove(doctor);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ Cw11/Pharmacy/Models/DoctorDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy.Models
+{
+    public class DoctorDto
+    {
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+        [Required, MaxLength(100)]
+        public string Email { get; set; }
+    }
+}
